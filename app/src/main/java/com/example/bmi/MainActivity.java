48a904b0287b52f199e66d79d309154dd354b3b6
YSkyLand <lovely1999617@gmail.com>
2019-04-16 20:11:06 +0800

    v2

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText ET1 , ET2 , ET3 ;
+    private EditText ET1, ET2, ET3;
     private TextView txv;
 
     @Override
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     int size = 30;
 
-    public void response(View view){
+    public void response(View view) {
         ET1 = findViewById(R.id.editText);
         ET2 = findViewById(R.id.editText2);
         ET3 = findViewById(R.id.editText3);
@@ -29,36 +29,33 @@ public class MainActivity extends AppCompatActivity {
 
         float height = Float.parseFloat(ET2.getText().toString());
         float weight = Float.parseFloat(ET3.getText().toString());
-        float BMI1 = calculateBMI(height,weight);
+        float BMI1 = calculateBMI(height, weight);
         String BMI2 = String.valueOf(BMI1);
 
         txv.setTextSize(size);
-        txv.setText("user:"+ET1.getText().toString()+"hello\n"+"Your BMI　is :"+BMI2);
-        Toast.makeText(this,standardBMI(BMI1),Toast.LENGTH_LONG).show();
+        txv.setText("user:" + ET1.getText().toString() + "hello\n" + "Your BMI　is :" + BMI2);
+        Toast.makeText(this, standardBMI(BMI1), Toast.LENGTH_LONG).show();
     }
 
-    public float calculateBMI(float height , float weight){
-        float BMI = (float) (weight / Math.pow((height/100),2));
+    public float calculateBMI(float height, float weight) {
+        float BMI = (float) (weight / Math.pow((height / 100), 2));
         return BMI;
     }
 
-    public String standardBMI(float x){
+    public String standardBMI(float x) {
         String alert = "";
 
-        if (x > 25){
+        if (x > 25) {
             alert = String.valueOf("你很重呢~真是可愛呢~");
             getWindow().setBackgroundDrawableResource(R.drawable.pig);
-        }
-
-        else if (x < 18.5){
+        } else if (x < 18.5) {
             alert = String.valueOf("豆芽菜很好吃，但希望你不要像豆芽菜一樣");
             getWindow().setBackgroundDrawableResource(R.drawable.vegetable);
-        }
-        else{
+        } else {
             alert = String.valueOf("你真的好棒棒喔!");
             getWindow().setBackgroundDrawableResource(R.drawable.sogreat);
         }
+        return alert;
     }
-
 
 }
